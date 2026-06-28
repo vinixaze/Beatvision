d0ec57d992b657ef5ab3eb4f0b4cd7a201f0b627
vinixaze <vini.aze2007@gmail.com>
2026-06-28 01:33:10 -0300

    Setup project architecture

---
+++ vision-engine/src/beatvision/app.py
@@ -0,0 +1,9 @@
+class App:
+    def __init__(self):
+        print("=" * 50)
+        print("BeatVision")
+        print("Virtual Drum Kit using Computer Vision")
+        print("=" * 50)
+
+    def run(self):
+        print("Application started successfully!")
